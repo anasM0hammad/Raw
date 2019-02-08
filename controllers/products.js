ff42da60ffe0f031b03b0e6a9b1d81abc324d2a4
anasM0hammad <anas.1633.m@gmail.com>
2019-02-08 19:09:16 +0530

    using files to store data

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,6 @@
 
-//PRODUCTS ARRAY
-const products = [] ;
+//PRODUCT MODEL
+const ProductModel = require('../models/productModel');
 
 // CONTROLLER FUNCTION TO RENDER THE ADD PRODUCT VIEW PAGE
 exports.getAddProduct = (req , res ,next) => {
@@ -9,13 +9,16 @@ exports.getAddProduct = (req , res ,next) => {
 
 //CONTROLLER FUNCTION TO TAKE THE ADD PRODUCT POST REQUEST
 exports.postAddProduct = (req , res , next) => {
-    const product = req.body.product ;
-    products.push({title : product}) ;
+    const product = new ProductModel(req.body.product) ;
+    product.save();
     res.redirect('/shop');
 }
 
 
 //CONTROLLER FUNCTION TO RENDER ALL PRODUCTS ON SHOP PAGE
 exports.getProducts = (req , res , next) => {
-    res.render('shop' , {docTitle : 'Shop' , prods : products });
+    ProductModel.fetchAll(products =>{
+        res.render('shop' , {docTitle : 'Shop' , prods : products });
+    });
+    
 }
--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,8 @@
-const products = [] ;
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../util/path');
+const productFile = path.join(rootDir , 'data' , 'products.json');
 
 module.exports = class Product {
    
@@ -8,11 +12,29 @@ module.exports = class Product {
    
     //METHOD TO SAVE THE PRODUCT IN PRODUCTS ARRAY
     save(){
-      products.push(this);
+      let products = [];  
+      fs.readFile(productFile , (err , fileContent) =>{
+            if(!err){
+                products = JSON.parse(fileContent);
+                products.push(this);
+            }
+
+            fs.writeFile(productFile , JSON.stringify(products) , err =>{
+                console.log(err);
+            });
+      });
+
     }
 
     //STATIC METHOD TO FETCH PRODUCTS ARRAY
-    static fetchAll(){
-        return products
+    static fetchAll(cb){
+        fs.readFile(productFile , (err , fileContent) => {
+          if(!err){
+             cb(JSON.parse(fileContent));
+          }
+          else{
+              cb([]);
+          }
+        });
     }
 }

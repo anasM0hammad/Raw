4b47c43ceb85b78865cb5862bb3634e4362b10d3
anasM0hammad <anas.1633.m@gmail.com>
2019-02-10 03:33:09 +0530

    creating cart page route functionality

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -13,6 +13,13 @@ exports.getCart = (req , res , next ) =>{
   res.render('shop/cart' , {docTitle: 'Cart' , path: '/cart'});
 }
 
+//CONTROLLER FUNCTION TO TAKE CART 
+exports.postCart = (req , res , next) => {
+    const productId = req.body.productId;
+    console.log(productId);
+    res.redirect('/');
+}
+
 //CONTROLLER FUNCTION TO RENDER CHECKOUT PAGE
 exports.getCheckout = (req , res , next ) =>{
     res.render('shop/checkout' , {docTitle: 'Checkout' , path: '/checkout'});
--- routes/shop.js
+++ routes/shop.js
@@ -13,6 +13,9 @@ router.get('/shop', shopController.getProducts);
 //ROUTE TO GET CART
 router.get('/cart' , shopController.getCart);
 
+//ROUTE FOR POST CART REQUEST
+router.post('/cart' , shopController.postCart);
+
 //ROUTE TO GET CHECKOUT
 router.get('/checkout' , shopController.getCheckout);
 

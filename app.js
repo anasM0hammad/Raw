a603ecf23fab3207f0ee0df816240ac09248d470
anasM0hammad <anas.1633.m@gmail.com>
2019-02-15 22:46:14 +0530

    using mongoose

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express') ;
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const path = require('path');
 
- const adminRouter = require('./routes/admin');
-const shopRouter = require('./routes/shop');
-const homeRouter = require('./routes/home');
+// const adminRouter = require('./routes/admin');
+// const shopRouter = require('./routes/shop');
+// const homeRouter = require('./routes/home');
 const notFoundRouter = require('./routes/404');
 
-const mongoConnect = require('./util/database').mongoConnect;
-const UserModel = require('./models/userModel') ;
+
+//const UserModel = require('./models/userModel') ;
 
 const app = express();
 
@@ -18,28 +19,33 @@ app.set('view engine' , 'ejs');
 app.use(express.static(path.join(__dirname , 'public')));
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req , res , next ) => {
-  UserModel.findById('5c6567971c9d440000c308b8')
-  .then(user => {
-    req.user = new UserModel(user.name , user.email , user.cart, user._id) ;
-    next();
-  })
-  .catch(err =>{
-    console.log(err);
-  });
-});
+// app.use((req , res , next ) => {
+//   UserModel.findById('5c6567971c9d440000c308b8')
+//   .then(user => {
+//     req.user = new UserModel(user.name , user.email , user.cart, user._id) ;
+//     next();
+//   })
+//   .catch(err =>{
+//     console.log(err);
+//   });
+// });
 
-app.use(homeRouter);
+// app.use(homeRouter);
 
-app.use(shopRouter);
+// app.use(shopRouter);
 
-app.use('/admin' , adminRouter);
+// app.use('/admin' , adminRouter);
 
 app.use(notFoundRouter);
 
-mongoConnect(client => {
+mongoose.connect('mongodb+srv://anasM0hammad:dWqmd6zjdCD1mV4D@raw-bawen.mongodb.net/raw?retryWrites=true')
+.then(result => {
+  console.log('CONNECTED');
   app.listen(3000);
-});
+})
+.catch(err => {
+  console.log(err);
+})
 
 
 //INITIALIZING TABLES IN DATABASE
